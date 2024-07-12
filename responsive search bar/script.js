c515d59fbeb64616b173709005effb32bf3633ad
Gopivellingiri <designergopi.cbe@gmail.com>
2024-07-12 05:37:27 +0530

    initial commit

---
+++ responsive search bar/script.js
@@ -0,0 +1,7 @@
+
+let inputBox = document.querySelector('.input-box'),
+search = document.querySelector('.search'),
+closeIcon = document.querySelector('.close-icon')
+
+search.addEventListener('click', () => inputBox.classList.add('open'))
+closeIcon.addEventListener('click', () => inputBox.classList.remove('open'))
